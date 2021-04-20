eeb319b3bb9e477f39a8cf81848cf671e0302369
Kalev Lember <klember@redhat.com>
2021-04-20 09:14:00 +0200

    Add gvfs-client to the runtime
    We didn't look at gio modules when generating the list of packages to
    include. This commit adds /usr/lib/x86_64-linux-gnu/gio/modules/ to the
    paths to sync from upstream, which ends up nicely pulling in
    gvfs-client.
    
    https://github.com/fedora-silverblue/issue-tracker/issues/108

--- tools/list-files.py
+++ tools/list-files.py
@@ -37,6 +37,7 @@ for d in python_dirs:
 
 output_dir_recurse('/usr/share/fonts')
 output_dir_recurse('/usr/share/themes')
+output_dir_recurse('/usr/lib/x86_64-linux-gnu/gio/modules/')
 output_dir_recurse('/usr/lib/x86_64-linux-gnu/gstreamer-1.0/')
 
 for d in ('gtk-2.0', 'gtk-3.0'):
