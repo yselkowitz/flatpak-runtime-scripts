f63703b3341a935c51d0458c001e33fd415b223e
Yaakov Selkowitz <yselkowi@redhat.com>
2023-03-24 12:26:04 -0400

    Detect gtk4 immodules

--- tools/list-files.py
+++ tools/list-files.py
@@ -50,9 +50,12 @@ output_dir_recurse('/usr/lib/x86_64-linux-gnu/gio/modules/')
 output_dir_recurse('/usr/lib/x86_64-linux-gnu/gstreamer-1.0/')
 output_dir_recurse('/usr/lib/x86_64-linux-gnu/sasl2/')
 
-for d in ('gtk-2.0', 'gtk-3.0'):
-    for v in os.listdir(os.path.join('/usr/lib/x86_64-linux-gnu/', d)):
-        output_dir_recurse(os.path.join('/usr/lib/x86_64-linux-gnu/', d, v, 'immodules'))
+for d in ('gtk-2.0', 'gtk-3.0', 'gtk-4.0'):
+    try:
+        for v in os.listdir(os.path.join('/usr/lib/x86_64-linux-gnu/', d)):
+            output_dir_recurse(os.path.join('/usr/lib/x86_64-linux-gnu/', d, v, 'immodules'))
+    except FileNotFoundError:
+        pass
 
 if sdk:
     output_dir_recurse('/usr/include')
