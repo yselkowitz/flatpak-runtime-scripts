1f7e002eca95726442bfc9074be24735836c032a
Yaakov Selkowitz <yselkowi@redhat.com>
2024-09-18 12:14:15 -0400

    Update baseline to FD.o 24.08 and GNOME 47

--- tools/generate-files.sh
+++ tools/generate-files.sh
@@ -6,11 +6,11 @@ base=$(basename $out)
 case $base in
     freedesktop-*)
 	ns=org.freedesktop
-	version=23.08
+	version=24.08
 	;;
     gnome-*)
 	ns=org.gnome
-	version=46
+	version=47
 	;;
     *)
 	echo 1>&2 "Can't identify runtime for $base"
