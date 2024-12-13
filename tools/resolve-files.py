c4c12430909b402cf515cec43f5be6a9b4a4f99b
Yaakov Selkowitz <yselkowi@redhat.com>
2024-12-13 15:11:44 -0500

    Do not exclude gnutls-utils
    This is required by swtpm-tools, which is a dependency of
    libvirt-daemon-driver-qemu for TPM emulation (e.g. for Windows 11
    guests) for virt-manager and gnome-boxes.

--- tools/resolve-files.py
+++ tools/resolve-files.py
@@ -554,7 +554,6 @@ global_package_ignore_patterns = [
     '^giflib-utils$',
     '^gitk$',
     '^glibc-utils$',
-    '^gnutls-utils$',
     '^gstreamer1-plugins-base-tools$',
     '^gtksourceview5-tests$',
     '^idn2$',
